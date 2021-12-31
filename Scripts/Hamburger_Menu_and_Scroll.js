f9f5fe33f52b2a26b41cb136519c50e88f7dd2e7
Jamesley <jamesdoxaint@gmail.com>
2022-01-01 00:14:43 +0100

    add hamburger menu slide animation

---
+++ Scripts/Hamburger_Menu_and_Scroll.js
@@ -0,0 +1,49 @@
+var menuBtn = document.querySelector('.menu-btn');
+var nav_bar_ul = document.getElementById('nav_bar_ul');
+let menuOpen = false;
+
+var localisation_a = document.getElementById("nav_localisation");
+var activities_a = document.getElementById("nav_activities");
+var contact_a = document.getElementById("nav_contact");
+
+var localisation = document.getElementById("maps");
+var activities = document.getElementById("activities_container");
+var contact = document.getElementById("contact_container");
+
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    nav_bar_ul.classList.add('active');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    nav_bar_ul.classList.remove('active');
+    menuOpen = false;
+  }
+  
+});
+
+
+activities_a.addEventListener("click", (e) => {
+  menuBtn.classList.remove('open');
+  nav_bar_ul.classList.remove('active');
+  menuOpen = false;
+  e.preventDefault();
+  activities.scrollIntoView({ behavior:'smooth', block:'start'});
+});
+
+contact_a.addEventListener("click", (e) => {
+  menuBtn.classList.remove('open');
+  nav_bar_ul.classList.remove('active');
+  menuOpen = false;
+  e.preventDefault();
+  contact.scrollIntoView({ behavior:'smooth', block:'start'});
+});
+
+localisation_a.addEventListener("click", (e) => {
+  menuBtn.classList.remove('open');
+  nav_bar_ul.classList.remove('active');
+  menuOpen = false;
+  e.preventDefault();
+  localisation.scrollIntoView({ behavior:'smooth', block:'start'})
+});
